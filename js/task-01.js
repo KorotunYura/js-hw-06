48813583e8b9bd52d8b251287d98b6a3e399914d
KorotunYura <yurakorotun1991@gmail.com>
2023-10-23 16:58:33 +0300

    update task_1

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const item = document.querySelectorAll('.item');
+console.log(item);
+console.log('Number of categories:', item.length);
+
+item.forEach(item => {
+  console.log(item);
+  console.log('Category:', item.firstElementChild.textContent);
+  console.log('Elements:', item.lastElementChild.children.length);
+});
