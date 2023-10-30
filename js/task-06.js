fd93740e0e24b6beb32ff9884c853a1c1614d252
KorotunYura <yurakorotun1991@gmail.com>
2023-10-30 22:37:02 +0200

    update task_6

---
+++ js/task-06.js
@@ -0,0 +1,27 @@
+const elements = {
+  input: document.querySelector('#validation-input'),
+};
+
+console.log(elements.input);
+
+elements.input.addEventListener('blur', handlerValues);
+
+function handlerValues(evt) {
+  console.dir(evt.currentTarget.value);
+  const { length } = evt.currentTarget.dataset;
+
+  if (evt.currentTarget.value.length === Number(length)) {
+    evt.currentTarget.classList.remove('invalid');
+    evt.currentTarget.classList.add('valid');
+    return;
+  }
+
+  if (evt.currentTarget.value.length === 0) {
+    evt.currentTarget.classList.remove('invalid');
+    evt.currentTarget.classList.remove('valid');
+    return;
+  }
+
+  evt.currentTarget.classList.remove('valid');
+  evt.currentTarget.classList.add('invalid');
+}
