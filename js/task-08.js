7477311d6b859bf69b2b1a4759f02ac29ada943d
KorotunYura <yurakorotun1991@gmail.com>
2023-10-31 11:50:49 +0200

    fix task_8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const elements = {
+  form: document.querySelector('.login-form'),
+};
 
+console.log(elements.form);
+
+elements.form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(evt) {
+  evt.preventDefault();
+
+  const { password, email } = evt.currentTarget.elements;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  const objForm = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(objForm);
+
+  evt.currentTarget.reset();
+}
