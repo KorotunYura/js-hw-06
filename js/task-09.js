d6227595b66a4477a9e5c6e36218ff26349d0265
KorotunYura <yurakorotun1991@gmail.com>
2023-10-31 12:53:52 +0200

    fix task_9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const elements = {
+  button: document.querySelector('.change-color'),
+  span: document.querySelector('.color'),
+  body: document.querySelector('body'),
+};
+
+elements.button.addEventListener('click', handlerClick);
+
+function handlerClick(evt) {
+  elements.span.textContent = getRandomHexColor();
+  elements.body.style.backgroundColor = elements.span.textContent;
+}
